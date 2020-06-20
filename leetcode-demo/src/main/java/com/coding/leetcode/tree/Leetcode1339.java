3414017814b147dbb0670b55bbfffc7f8b8e35e2
wenjun shu <shu_wenjun@qq.com>
2020-06-20 14:37:04 +0800

    更新
    Signed-off-by: 舒文君 <shuwenjun@corp.netease.com>

--- leetcode-demo/src/main/java/com/coding/leetcode/tree/Leetcode1339.java
+++ leetcode-demo/src/main/java/com/coding/leetcode/tree/Leetcode1339.java
@@ -6,9 +6,42 @@ package com.coding.leetcode.tree;
  * @Description
  */
 public class Leetcode1339 {
-  public int maxProduct(TreeNode root) {
 
-    return 0;
+  int rootValue = 0;
+  long max = 0L;
+
+  public int maxProduct(TreeNode root) {
+    dfs(root);
+    this.rootValue = root.val;
+    dfs2(root);
+    return (int) (max % 1000000007);
+  }
+
+
+  private int dfs(TreeNode node) {
+    if (node == null) {
+      return 0;
+    }
+    int left = dfs(node.left);
+    int right = dfs(node.right);
+    int mid = left + right + node.val;
+    node.val = mid;
+    return mid;
+  }
+
+  private void dfs2(TreeNode node) {
+    if (node == null) {
+      return;
+    }
+    // 这里的rootValue表示的原始整个树的和值
+    this.max = Math.max(max, (long) (rootValue - node.val) * node.val);
+    dfs2(node.left);
+    dfs2(node.right);
+  }
+
+
+  public static void main(String[] args) {
+    Leetcode1339 leetcode1339 = new Leetcode1339();
   }
 }
 
---
+++ leetcode-demo/src/main/java/com/coding/leetcode/tree/Leetcode572.java
@@ -0,0 +1,9 @@
+package com.coding.leetcode.tree;
+
+/**
+ * @Author shu wj
+ * @Date 2020/6/20 14:36
+ * @Description
+ */
+public class Leetcode572 {
+}
